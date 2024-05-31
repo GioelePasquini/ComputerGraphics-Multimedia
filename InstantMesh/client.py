1b37bc43a176718053b32388e7f6d9f596c08e5c
Gioele <gioele.pasquini101@asu.ulpgc.es>
2024-05-31 14:19:01 +0200

    Aggiunto codice funzionante server (kaggle)

--- InstantMesh/client.py
+++ InstantMesh/client.py
@@ -1,13 +1,22 @@
-import requests
+from io import BytesIO
 
+import requests
+from PIL import Image
 # URL pubblico generato da ngrok, sostituire ogni volta
-url = 'https://ed83-34-16-133-6.ngrok-free.app/prova'
+url = 'https://e04d-34-83-243-250.ngrok-free.app/mao'
 
 # Percorso dell'immagine da elaborare
 image_path = '/content/drive/MyDrive/cute_tiger.jpg'
+prompt = "cowboy with a white hat"
+#data = {'image_path': image_path}
+data = {'prompt': prompt}
 
-data = {'image_path': image_path}
+response = requests.post(url, json={'prompt': prompt})
 
-response = requests.post(url, json=data)
-
-print(response.json())
+if response.status_code == 200:
+    # Crea un oggetto Image da PIL
+    img = Image.open(BytesIO(response.content))
+    # Visualizza l'immagine
+    img.show()
+else:
+    print(f"Failed to retrieve image. Status code: {response.status_code}")
