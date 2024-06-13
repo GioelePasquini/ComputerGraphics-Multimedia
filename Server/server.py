096a0ba4cc87e5b76d775e8530ac23c553ffdaad
AndreaCiv <75083105+AndreaCiv@users.noreply.github.com>
2024-06-13 12:47:32 +0200

    Server modificato
    Aggiunta stampa delle richieste ricevute e metodo di salvataggio dei file generati

--- Server/server.py
+++ Server/server.py
@@ -8,14 +8,12 @@ import zipfile
 
 if __name__ == '__main__':
 
-    ngrok.set_auth_token("2hCAtAEtjMYj0RzyLs4V8lN3QE7_6fg61ry6eQuCWnpeynvSL")
+    ngrok.set_auth_token("2h5uqCeg949GltwEdN8VJXI2JZu_6EoRANAJJnHon5k6HTC53")
     logging.basicConfig(level=logging.INFO)
     listener = ngrok.werkzeug_develop()
     print(listener.url())
     app = flask.Flask(__name__)
 
-    # Indice per nominare i diversi file restituiti dal server
-    counter = 1
 
     # Paths
     path_server = "/kaggle/working/output"
@@ -36,18 +34,18 @@ if __name__ == '__main__':
 
     @app.route('/image_to_model', methods=['POST'])
     def image_to_model():
-        global counter, path_server
+        global path_server
         data = request.json
         image_path = data['image_path']
-        file_name = "model_generated_"+str(counter)
+        print("Richiesta image to model ricevuta")
+        file_name = "model_generated"
         output_path = path_server+"/InstantMesh/"+file_name
         command = f"python /kaggle/working/InstantMesh/run.py configs/instant-mesh-base.yaml {image_path} --export_texmap --output_path {output_path}"
         try:
             subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
-            print("Modello n. " + str(counter) + " creato e salvato sul server. Ora li invio.")
+            print("Modello della richiesta creato e salvato sul server. Ora li invio.")
             zip_stream = zip_files_in_directory(output_path+"/instant-mesh-base/meshes")
             zip_stream.seek(0)
-            counter += 1
             return send_file(zip_stream, mimetype='application/zip', download_name='files.zip', as_attachment=True)
 
         except subprocess.CalledProcessError as e:
@@ -56,18 +54,18 @@ if __name__ == '__main__':
 
     @app.route('/text_to_image', methods=['POST'])
     def text_to_image():
-        global counter, path_server
+        global path_server
         data = request.json
         prompt = data['prompt']
-        file_name = "img_generated_"+str(counter)
-        output_path = path_server+"/text_to_image/"+file_name
+        print("Richiesta text to image ricevuta")
+        file_name = "img_generated"
+        output_path = path_server+"/images/"+file_name
         command = f"python /kaggle/input/text-to-image/text_to_image.py --prompt \"{prompt}\" --output_path {output_path}"
         try:
             subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
-            print("4 immagini relative alla n. " + str(counter) + " create e salvate sul server. Ora le invio.")
+            print("4 immagini relative alla richiesta create e salvate sul server. Ora le invio.")
             zip_stream = zip_files_in_directory(output_path)
             zip_stream.seek(0)
-            counter += 1
             return send_file(zip_stream, mimetype='application/zip', download_name='files.zip', as_attachment=True)
 
         except subprocess.CalledProcessError as e:
@@ -78,19 +76,19 @@ if __name__ == '__main__':
 
     @app.route('/image_to_image', methods=['POST'])
     def image_to_image():
-        global counter, path_server
+        global path_server
         data = request.json
         prompt = data['prompt']
         input_path = data['input_path']
-        file_name = "img_generated_" + str(counter)
-        output_path = path_server + "/image_to_image/" + file_name
+        print("Richiesta image to image ricevuta")
+        file_name = "img_generated"
+        output_path = path_server + "/images/" + file_name
         command = f"python /kaggle/input/image-to-image/image_to_image.py --prompt \"{prompt}\" --output_path {output_path} --input_path {input_path}"
         try:
             subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
-            print("4 immagini relative alla n. " + str(counter) + " create e salvate sul server. Ora le invio.")
+            print("4 immagini relative alla richiesta create e salvate sul server. Ora le invio.")
             zip_stream = zip_files_in_directory(output_path)
             zip_stream.seek(0)
-            counter += 1
             return send_file(zip_stream, mimetype='application/zip', download_name='files.zip', as_attachment=True)
 
         except subprocess.CalledProcessError as e:
